5311303a71d050b045156cebcb0ccef75ce5922e
Benjamin Cisneros Barraza <cisnerosbarraza@gmail.com>
2025-10-19 16:36:15 -0600

    initial project structure

---
+++ src/test/java/demo/legacy/LegacyPriceCalculatorTest.java
@@ -0,0 +1,13 @@
+
+package demo.legacy;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LegacyPriceCalculatorTest {
+    
+    @Test
+    void test() {
+    }
+
+}
